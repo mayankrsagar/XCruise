11787980ad7cdc7e0d6499cd90addca6daa819a1
mayankrsagar <mayankrsagar@gmail.com>
2024-07-20 18:40:16 +0530

    “ME_FE_BUILDOUT_XCRUISE_MODULE_ONE”  (Crio.Do)

--- assessment/cypress/e2e/spec.cy.js
+++ assessment/cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 describe('xCruise Landing Page Tests', () => {
   beforeEach(() => {
     // Visit the xCruise home page before each test
-    cy.visit('http://localhost:3000/') // Replace with the actual URL
+    cy.visit('http://3.110.47.78:8081/') // Replace with the actual URL
   });
 
   it('Navigation Bar - should display the xCruise logo', () => {
